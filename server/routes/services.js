2d7fd29c6c66398488ad89aa4bf9489c7e49923e
Zoya <romanova.zoya.2002@mail.ru>
2022-11-09 12:08:43 +0300

    d k -

--- server/routes/services.js
+++ server/routes/services.js
@@ -4,8 +4,8 @@ const {isAuth} = require('../middleware/auth')
 
 const controller = require('../controllers/services');
 
-router.get('/good-college-summer', controller.dkTest)
-router.post('/good-college-summer', controller.dkCheck)
+// router.get('/good-college-summer', controller.dkTest)
+// router.post('/good-college-summer', controller.dkCheck)
 router.get('/', controller.getServicesListPage) 
 router.get('/:path', controller.getServicePage)
 router.post('/:id/like', isAuth, controller.toggleLike)
