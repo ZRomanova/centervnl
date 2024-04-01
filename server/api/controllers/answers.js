b47f3ba99388f242b1053fa25aae3ee7b028c51f
ZRomanova <romanova.zoya.2002@mail.ru>
2024-04-01 12:14:05 +0300

    chat bot ru

--- server/api/controllers/answers.js
+++ server/api/controllers/answers.js
@@ -5,11 +5,21 @@ module.exports.getAnswer = async function (req, res) {
     try {
         const data = await Base.aggregate(
             [
-                { $match: { $text: { $search: req.body.message } } },
+                {
+                    $match: {
+                        $text: {
+                            $search: req.body.message,
+                            $language: 'ru'
+                        }
+                    }
+                },
                 { $sort: { score: { $meta: "textScore" } } },
+                { $limit: 4 },
                 { $project: { _id: 0, keywords: 0 } }
             ]
         )
+
+        // console.log(data)
 
         res.status(200).json(data)
     } catch (e) {
